d0297982e2a672aaadf21ad3786684394b917caa
David Schoenholtz <dmschoenholtz@gmail.com>
2019-11-25 19:11:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,5 +18,11 @@ def assign_rooms(names)
   list
 end
 
-def printer(names)
+def printer(attendees)
+  batch_badge_creator(attendees).each do |attendee|
+    puts attendee
+  end
+  assign_rooms(attendees).each do |room_assignment|
+    puts room_assignment
+  end
 end
